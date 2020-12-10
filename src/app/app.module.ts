294c6018c4412b2368e450fcc9c26720114180d2
FonctionZ3R0 <donovan.damhec@gmail.com>
2020-12-10 17:13:28 +0100

    delete imagesearch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ImagesearchComponent } from './imagesearch/imagesearch.component';
 import { SwitchpageComponent } from './switchpage/switchpage.component';
 import { PrintimagesComponent } from './printimages/printimages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImagesearchComponent,
     SwitchpageComponent,
     PrintimagesComponent
   ],
